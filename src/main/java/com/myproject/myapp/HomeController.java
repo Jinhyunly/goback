90bbc6262d9dfacdc570715c8eec6e37342ab276
Jinhyunly <dbdbddipp@gmail.com>
2020-05-12 00:26:17 +0900

    Revert "Revert "TEST COMIIT 3""
    This reverts commit 1d2c1a771ba14879234cbe61e9103797e8ffcfad.

--- src/main/java/com/myproject/myapp/HomeController.java
+++ src/main/java/com/myproject/myapp/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome fuck japan test korea! The client locale is {}.", locale);
+		logger.info("Welcome fuck japan test korea! The client locale is !{}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);

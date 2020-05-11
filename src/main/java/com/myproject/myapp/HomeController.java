bcaf0e31231f8fb9b54da0865a8b6f561e0e55d3
Jinhyunly <dbdbddipp@gmail.com>
2020-05-12 03:00:48 +0900

    test4 lee

--- src/main/java/com/myproject/myapp/HomeController.java
+++ src/main/java/com/myproject/myapp/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		logger.info("Welcome lee jinhyun test3  commit The client locale is {}.", locale);
+		logger.info("Welcome lee jinhyun test4 commit The client locale is {}.", locale);
 
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
